35f390486d0e47383781226ef9983cdda7ff00ba
Nicolás Venturo <nicolas.venturo@gmail.com>
2016-09-10 18:32:26 -0300

    The turtle is now commanded by our node, which tracks its position

--- turtle_draw/catkin_ws/src/turtle_draw/src/turtle_draw.py
+++ turtle_draw/catkin_ws/src/turtle_draw/src/turtle_draw.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist, Point
+from turtlesim.msg import Pose
+
+turtle_pose = Pose()
 
 def main():
-    rospy.init_node("logger", anonymous=True)
-    rate = rospy.Rate(1) # 10 Hz
+    rospy.init_node("turtle_draw")
+
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rospy.Subscriber("/turtle1/pose", Pose, pose_callback)
+
+    rate = rospy.Rate(10) # 10 Hz
 
     while not rospy.is_shutdown():
-        hello_str = "Hello world! Time: %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
+        t = Twist(linear = Point(2 , 2, 0), angular = Point(0, 0, 5))
+        pub.publish(t)
+
+        rospy.loginfo("pose = x: %d y: %d theta: %d" % (turtle_pose.x, turtle_pose.y, turtle_pose.theta))
+
         rate.sleep()
+
+def pose_callback(pose):
+    global turtle_pose
+    turtle_pose = pose
 
 if __name__ == "__main__":
     main()

1d4dad4f40c13c717610224aa89c93e474139ad1
Nicolás Venturo <nicolas.venturo@gmail.com>
2016-09-11 01:47:07 -0300

    Tons of improvements! We now correct the angle while moving forward, and spin in the best direction

--- turtle_draw/catkin_ws/src/turtle_draw/src/turtle_draw.py
+++ turtle_draw/catkin_ws/src/turtle_draw/src/turtle_draw.py
@@ -4,7 +4,8 @@ import rospy
 from geometry_msgs.msg import Twist, Point
 from turtlesim.msg import Pose
 
-from math import atan, pi
+from math import atan2, sqrt, pi
+import time
 
 # The turtle's position - this is set by the subsriber callback
 turtle_pose = Pose()
@@ -17,63 +18,88 @@ def main():
 
     rate = rospy.Rate(10) # 10 Hz
 
-    move_straight(Pose(x=8, y=8), 1, 0.1, rate, pub)
+    time.sleep(0.5) # We need to sleep for a bit to let the subscriber fetch the current pose at least once
+
+    points = [[8, 8], [5, 10], [2, 8], [0, 5], [2, 2], [5, 0], [8, 2], [10, 5], [8, 8]]
+    for point in points:
+        move_straight(Pose(x=point[0], y=point[1]), 1, 1, 0.1, rate, pub)
 
     rospy.loginfo("We're done!")
     while not rospy.is_shutdown():
         rate.sleep()
 
-
 def pose_callback(pose):
     global turtle_pose
     turtle_pose = pose
 
-def move_straight(target, speed, tolerance, rate, pub):
+def move_straight(target, move_speed, spin_speed, pos_tolerance, rate, pub):
     rospy.loginfo("Going to x: %.2f y: %.2f" % (target.x, target.y))
 
     # We first spin, then move forward
 
-    target_theta = angle_between_points(target, turtle_pose)
+    target_theta = angle_between_points(turtle_pose, target)
 
     rospy.loginfo("Need to aquire theta: %.2f" % target_theta)
-    while not are_angles_equal(turtle_pose.theta, target_theta, deg_to_rad(1)) and not rospy.is_shutdown():
-        spin(speed, target_theta < 0, pub)
+    while not are_angles_equal(turtle_pose.theta, target_theta, deg_to_rad(5)) and not rospy.is_shutdown():
+        spin(spin_speed, is_spin_clockwise(turtle_pose.theta, target_theta), pub)
         rate.sleep()
 
     rospy.loginfo("Done spinning!")
     stop(pub) # Stop spinning
 
     rospy.loginfo("Moving to the target")
-    while not are_points_equal(turtle_pose, target, 0.2, coords=["x", "y"]) and not rospy.is_shutdown():
-        advance(speed, True, pub)
-        rate.sleep()
+    # We don't move in a straight line because we might need to do small angle corrections
+    move(target, move_speed, spin_speed, pos_tolerance, rate, pub)
 
     rospy.loginfo("Reached the target!")
     stop(pub) # Stop moving
 
-def angle_between_points(point_a, point_b):
-    if abs(point_b.x - point_a.x) < 0.0001: # These are floating point numbers, so we don't check for equality directly
-        if point_b.y - point_a.y > 0:
-            return pi/2
-        else:
-            return -pi/2
-    else:
-        return atan((point_b.y - point_a.y) / (point_b.x - point_a.x))
+def move(target, move_speed, spin_speed, pos_tolerance, rate, pub):
+    while not are_points_equal(turtle_pose, target, pos_tolerance) and not rospy.is_shutdown():
+        # We move forward, but may need to apply small angle corrections while doing so
+        target_theta = angle_between_points(turtle_pose, target)
+        angle_correction = min_angle_between_angles(turtle_pose.theta, target_theta)
 
-def are_points_equal(point_a, point_b, tolerance, coords=["x", "y", "z"]):
-    return all([abs(getattr(point_a, coord) - getattr(point_b, coord)) < tolerance for coord in coords])
+        # We could apply some proportional gain to angle_correction here, but too much
+        # will make us zigzag
+        target_spin_speed = clamp(angle_correction, -spin_speed, spin_speed)
 
-def are_angles_equal(angle_a, angle_b, tolerance):
-    return abs(normalize_rad(angle_a) - normalize_rad(angle_b)) < tolerance
+        pub.publish(Twist(linear=Point(move_speed, 0, 0), angular=Point(0, 0, target_spin_speed)))
+        rate.sleep()
 
 def spin(speed, clockwise, pub):
     pub.publish(Twist(linear=Point(0, 0, 0), angular=Point(0, 0, speed * (-1 if clockwise else 1))))
 
-def advance(speed, forward, pub):
-    pub.publish(Twist(linear=Point(speed * (1 if forward else -1), 0, 0), angular=Point(0, 0, 0)))
-
 def stop(pub):
-    advance(0, True, pub)
+    pub.publish(Twist(linear=Point(0, 0, 0), angular=Point(0, 0, 0)))
+
+def angle_between_points(point_a, point_b):
+    # atan2 works correctly on all 4 quadrants, and we don't need to
+    # worry about dividing by zero when the x coordinates match
+    return atan2((point_b.y - point_a.y), (point_b.x - point_a.x))
+
+def min_angle_between_angles(angle_a, angle_b):
+    # First, we normalize both angles so that we're working in the same bounded range
+    angle_a = normalize_rad(angle_a)
+    angle_b = normalize_rad(angle_b)
+
+    # There are two other candidates for angle_a that we neeed to consider: the
+    # immediately inferior and the immediately superior
+    possible_angle_as = [angle_a - 2 * pi, angle_a, angle_a + 2 * pi]
+    angle_differences = [angle_b - possible_angle_a for possible_angle_a in possible_angle_as]
+
+    # The angle difference that we want is the one with the lowest absolute value
+    min_abs_diff, idx = min((abs(diff), idx) for (idx, diff) in enumerate(angle_differences))
+    return angle_differences[idx]
+
+def is_spin_clockwise(current_angle, target_angle):
+    return min_angle_between_angles(current_angle, target_angle) < 0
+
+def are_points_equal(point_a, point_b, tolerance):
+    return sqrt((point_a.x - point_b.x) ** 2 + (point_a.y - point_b.y) ** 2) < tolerance
+
+def are_angles_equal(angle_a, angle_b, tolerance):
+    return abs(normalize_rad(angle_a) - normalize_rad(angle_b)) < tolerance
 
 def deg_to_rad(deg):
     return pi * deg / 180
@@ -84,6 +110,9 @@ def normalize_rad(rad):
     while rad <= -pi:
         rad += 2 * pi
     return rad
+
+def clamp(val, min_val, max_val):
+    return max(min_val, min(val, max_val))
 
 if __name__ == "__main__":
     main()

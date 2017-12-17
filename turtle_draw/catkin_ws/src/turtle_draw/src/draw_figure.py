aad33f501d34a4f85149bd08bcadd591a2dfbad2
Nicolás Venturo <nicolas.venturo@gmail.com>
2017-12-17 18:07:27 -0300

    Added some tests, working on how to organize them

--- turtle_draw/catkin_ws/src/turtle_draw/src/draw_figure.py
+++ turtle_draw/catkin_ws/src/turtle_draw/src/draw_figure.py
@@ -102,6 +102,7 @@ def _are_angles_equal(angle_a, angle_b, tolerance):
     return abs(_normalize_rad(angle_a) - _normalize_rad(angle_b)) < tolerance
 
 def _normalize_rad(rad):
+    # Normalization to (-pi, pi]
     while rad > math.pi:
         rad -= 2 * math.pi
     while rad <= -math.pi:
@@ -116,15 +117,57 @@ def _clamp(val, min_val, max_val):
     return max(min_val, min(val, max_val))
 
 class _TestHelperFunctions(unittest.TestCase):
-    def test_clamp(self):
-        self.assertEqual(_clamp(10, 1, 100), 10)
-        self.assertEqual(_clamp(10, 20, 100), 20)
-        self.assertEqual(_clamp(10, 1, 5), 5)
-        with self.assertRaises(ValueError):
-            _clamp(10, 1, 0)
+    def test_angle_between_points(self):
+        pass
+
+    def test_min_angle_between_angles(self):
+        pass
+
+    def test_is_spin_clockwise(self):
+        pass
+
+    def test_are_points_equal(self):
+        pass
+
+    def test_are_angles_equal(self):
+        pass
+
+    def test_normalize_rad(self):
+        # Normal usage
+        self.assertAlmostEqual(_normalize_rad(0), 0) # No offset
+        self.assertAlmostEqual(_normalize_rad(math.pi / 2), math.pi / 2) # No normalization needed (positive angle)
+        self.assertAlmostEqual(_normalize_rad(-math.pi / 2), -math.pi / 2) # No normalization needed (negative angle)
+
+        # Edge cases
+        self.assertAlmostEqual(_normalize_rad(math.pi), math.pi) # pi is not normalized
+        self.assertAlmostEqual(_normalize_rad(-math.pi), math.pi) # -pi is normalized
+
+        # Extreme cases
+        self.assertAlmostEqual(_normalize_rad(2 * math.pi), 0) # Large positive angle
+        self.assertAlmostEqual(_normalize_rad(-2 * math.pi), 0) # Large negative angle
+        self.assertAlmostEqual(_normalize_rad(20 * math.pi), 0) # Very large positive angle
+        self.assertAlmostEqual(_normalize_rad(-20 * math.pi), 0) # Very large negative angle
 
     def test_deg_to_rad(self):
-        self.assertEqual(_deg_to_rad(0), 0)
+        # Normal usage
+        self.assertAlmostEqual(_deg_to_rad(0), 0) # No offset
+        self.assertAlmostEqual(_deg_to_rad(180), math.pi) # Normal case
+        self.assertAlmostEqual(_deg_to_rad(-180), -math.pi) # Negative angles
+
+        # Extreme cases
+        self.assertAlmostEqual(_deg_to_rad(180 + 360), 3 * math.pi) # Angles over 360 degrees
+        self.assertAlmostEqual(_deg_to_rad(-180 - 360), -3 * math.pi) # Negative Angles over 360 degrees
+
+    def test_clamp(self):
+        # Normal usage
+        self.assertEqual(_clamp(10, 1, 100), 10) # No clamping
+        self.assertEqual(_clamp(10, 20, 100), 20) # Clamping at bottom
+        self.assertEqual(_clamp(10, 1, 5), 5) # Clamping at top
+        self.assertEqual(_clamp(10, 5, 5), 5) # Clamping at both
+
+        # Bad usage
+        with self.assertRaises(ValueError):
+            _clamp(10, 1, 0) # Max smaller than min
 
 if __name__ == "__main__":
     print "Testing draw_figure: for normal usage, import the module"
